b780d1b02ca94b64a4976e39ff71f96bad241116
pygenicarc <pygenicarc@gmail.com>
2025-10-25 23:16:12 +0530

    test

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, jsonify, request, render_template, redirect, url_for
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-# CI trigger: small comment added to cause a push event
+app = Flask("__name__")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
 
-app = Flask('__name__')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:Reddy%4096320@localhost:5433/amazon_app'
-
+# testing change
 db = SQLAlchemy(app)
 
 
@@ -21,7 +19,7 @@ with app.app_context():
     db.create_all()
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     data = Todo.query.all()
     context = []
@@ -30,64 +28,68 @@ def index():
         context.append(dd)
     print(context)
     # print("data: {}".format(data))
-    return render_template('todo.html', todo=context)
+    return render_template("todo.html", todo=context)
 
 
-@app.route('/add-task')
+@app.route("/add-task")
 def add_task():
-    return render_template('add_task.html')
+    return render_template("add_task.html")
 
 
-@app.route('/submit', methods=['POST'])
+@app.route("/submit", methods=["POST"])
 def create_user():
-    title = request.form['title']
-    task = request.form['task']
-    due = request.form['due']
+    title = request.form["title"]
+    task = request.form["task"]
+    due = request.form["due"]
     print(f"title is: {title}, task is: {task}, and due is: {due}")
     new_task = Todo(title=title, task=task, due=due)
     print("new_task: {}".format(new_task))
     db.session.add(new_task)
     db.session.commit()
-    return redirect(url_for('add_task'))
+    return redirect(url_for("add_task"))
 
 
-@app.route('/delete/<int:id>', methods=['GET', 'DELETE'])
+@app.route("/delete/<int:id>", methods=["GET", "DELETE"])
 def delete_user(id):
     task = Todo.query.get(id)
     print("task: {}".format(task))
 
     if not task:
-        return jsonify({'message': 'task not found'}), 404
+        return jsonify({"message": "task not found"}), 404
     try:
         db.session.delete(task)
         db.session.commit()
-        return jsonify({'message': 'task deleted successfully'}), 200
+        return jsonify({"message": "task deleted successfully"}), 200
     except Exception as e:
         db.session.rollback()
-        return jsonify({'message': 'An error occurred while deleting the data {}'.format(e)}), 500
+        return (
+            jsonify({"message": "An error occurred {}".format(e)}),
+            500,
+        )
 
 
-@app.route('/update_task/<int:id>', methods=['GET', 'POST'])
+@app.route("/update_task/<int:id>", methods=["GET", "POST"])
 def update_task(id):
     task = Todo.query.get_or_404(id)
     print(task.id)
     if not task:
-        return jsonify({'message': 'task not found'}), 404
+        return jsonify({"message": "task not found"}), 404
 
-    if request.method == 'POST':
-        task.title = request.form['title']
-        task.task = request.form['task']
-        task.due = request.form['due']
+    if request.method == "POST":
+        task.title = request.form["title"]
+        task.task = request.form["task"]
+        task.due = request.form["due"]
 
         try:
             db.session.commit()
-            return redirect(url_for('index'))
+            return redirect(url_for("index"))
 
         except Exception as e:
+            print(e)
             db.session.rollback()
             return "there is an issue while updating the record"
-    return render_template('update.html', task=task)
+    return render_template("update.html", task=task)
 
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5002, debug=True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5002, debug=True)
